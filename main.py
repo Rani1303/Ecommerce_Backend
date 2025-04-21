376fdd348a1a2145b1d779efddd621d59a16e7c6
Rani <103280525+Rani1303@users.noreply.github.com>
2025-04-21 17:06:42 +0530

    Feat: Endpoints done

---
+++ main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+from typing import Optional
+from database import engine, get_db, Product, Base
+from pydantic import BaseModel
+
+class ProductBase(BaseModel):
+    name: str
+    description: Optional[str]=None
+    price: float
+    stock: int
+
+class ProductCreate(ProductBase):
+    pass
+
+class ProductResponse(ProductBase):
+    id: int
+
+    class Config:
+        from_attributes=True
+
+
+Base.metadata.create_all(bind=engine)
+app=FastAPI(title="Ecommerce API")
+
+@app.get("/products/", response_model=List[ProductResponse])
+def get_products(skip: int=0, limit: int=100, db: Session=Depends(get_db)):
+    products=db.query(Product).offset(skip).limit(limit).all()
+    return products
+
+@app.get("/products/{product_id}", response_model=ProductResponse)
+def get_product(product_id : int, db:Session=Depends(get_db)):
+    product=db.query(Product).filter(Product.id==product_id).first()
+    if product is None:
+        raise HTTPException(status_code=404, details="Product not found")
+    return product
+
+@app.post("/add_product/",response_model=ProductResponse)
+def add_product(product: ProductCreate, db: Session=Depends(get_db)):
+    db_product = Product(
+        name=product.name,
+        description=product.description,
+        price=product.price,
+        stock=product.stock
+    )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
+    
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
